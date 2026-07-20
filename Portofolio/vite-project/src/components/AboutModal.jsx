73022b9817b2849d7e639b81c90e819a28037d9d
Jeremy <jeremy.yosep@gmail.com>
2026-07-21 00:28:09 +0700

    UPDATES

--- Portofolio/vite-project/src/components/AboutModal.jsx
+++ Portofolio/vite-project/src/components/AboutModal.jsx
@@ -133,7 +133,7 @@ export default function AboutModal({ isOpen, onClose }) {
                   style={{ padding: 0, border: 'none', background: 'transparent' }}
                 >
                   <img 
-                    src="/images/IMG_20260720_203555.webp" 
+                    src="/images/pfp_formal (1).png" 
                     alt="Jeremy Yosep Pohar" 
                     style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
                   />
